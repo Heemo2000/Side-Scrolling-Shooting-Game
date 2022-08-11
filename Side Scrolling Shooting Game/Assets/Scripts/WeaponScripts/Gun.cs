c431033dad23924c7237cbb44e6bbe437d8b27a6
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-08-11 22:06:15 +0530

    Renamed Weapon Scripts folder to WeaponScripts.

--- Side Scrolling Shooting Game/Assets/Scripts/WeaponScripts/Gun.cs
+++ Side Scrolling Shooting Game/Assets/Scripts/WeaponScripts/Gun.cs
@@ -12,12 +12,12 @@ public class Gun : MonoBehaviour
 
     private float _nextTimeToFire = 0.0f;
 
-    public Action OnFire;
+    public Action OnBulletShoot;
     public void Fire()
     {
         if(_nextTimeToFire < Time.time)
         {
-            OnFire?.Invoke();
+            OnBulletShoot?.Invoke();
             Bullet bullet = Instantiate(bulletPrefab,firePoint.position,Quaternion.identity);
             bullet.transform.right = firePoint.right;
             _nextTimeToFire = Time.time + fireInterval;
