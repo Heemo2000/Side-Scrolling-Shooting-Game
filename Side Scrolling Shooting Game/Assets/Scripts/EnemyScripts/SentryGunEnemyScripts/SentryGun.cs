97e18ab480b22d9a15f2ff759a46086a49071a81
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-08-15 09:15:07 +0530

    Implemented sentry gun AI.

---
+++ Side Scrolling Shooting Game/Assets/Scripts/EnemyScripts/SentryGunEnemyScripts/SentryGun.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentryGun : BaseEnemy
+{
+    [SerializeField]private float lookOffSetY = 2.0f;
+    [SerializeField]private Transform firePoint;   
+    [Range(5f,100f)]
+    [SerializeField]private float targetCheckDistance = 6f;
+
+    [SerializeField]private LayerMask rayCastIgnoreLayerMask;
+    [SerializeField]private Gun gun;
+    private GenericAimHandler _aimHandler;
+
+
+
+    private void Awake() {
+        _aimHandler = GetComponent<GenericAimHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBehaviour();
+    }
+
+    protected override void HandleBehaviour()
+    {
+        _aimHandler.SetAimPosition(Target.position + Vector3.up * lookOffSetY);
+        HandleShooting();
+    }
+    private void HandleShooting()
+    {
+        
+        if(Physics.Raycast(firePoint.position,firePoint.right,out RaycastHit hit,targetCheckDistance,~rayCastIgnoreLayerMask.value))
+        {
+            Debug.Log("Hit object : " + hit.transform.name);
+            Debug.DrawLine(firePoint.position,hit.point);
+            if(hit.transform.gameObject.layer == Target.gameObject.layer)
+            {
+                gun.Fire();
+            }
+        }
+    }
+}
