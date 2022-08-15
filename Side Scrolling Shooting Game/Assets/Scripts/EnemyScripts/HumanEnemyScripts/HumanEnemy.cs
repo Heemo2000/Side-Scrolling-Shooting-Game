b0f3bd10c019a493c106f0dbbdb91b551c43b7f6
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-08-15 17:14:19 +0530

    Fixed double jump up animation triggering issue in Human enemy.

--- Side Scrolling Shooting Game/Assets/Scripts/EnemyScripts/HumanEnemyScripts/HumanEnemy.cs
+++ Side Scrolling Shooting Game/Assets/Scripts/EnemyScripts/HumanEnemyScripts/HumanEnemy.cs
@@ -18,7 +18,6 @@ public class HumanEnemy : BaseEnemy
     [SerializeField]private LayerMask collisionIgnoreLayerMask;
 
     [SerializeField]private LayerMask offMeshLinkLayerMask;
-    [SerializeField]private LayerMask groundLayerMask;
     [SerializeField]private Transform groundCheck;
 
     [Range(1f,10f)]
@@ -32,10 +31,11 @@ public class HumanEnemy : BaseEnemy
     [SerializeField]private Animator animator;
     [SerializeField]private ParticleSystem explosionEffect;
     [SerializeField]private Gun gun;   
-    public Animator Animator { get => animator;}
+    public Animator HumanAnimator { get => animator;}
     public ParticleSystem ExplosionEffect { get => explosionEffect;}
     public NavMeshAgent Agent { get => _agent;}
     public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+    public Health Health { get => _health; }
 
     private NavMeshAgent _agent;
     private GenericAimHandler _aimHandler;
@@ -44,14 +44,15 @@ public class HumanEnemy : BaseEnemy
 
     private HumanEnemyChaseState _chaseState;
     private HumanEnemyStopState _stopState;
+
+    private Health _health;
     private float _currentMoveAnimSpeed = 0;
 
     private bool _shouldStop = false;
-
-    private bool _isJumping = false;
     private void Awake() 
     {
         _agent = GetComponent<NavMeshAgent>();
+        _health = GetComponent<Health>();
         _aimHandler = GetComponent<GenericAimHandler>();
         _enemyStateMachine = new StateMachine();
         
@@ -77,27 +78,11 @@ public class HumanEnemy : BaseEnemy
 
         HandleFrontSide();
         HandleShooting();
-        HandleAnimation();
 
         _aimHandler.SetAimPosition(Target.position + Vector3.up * lookOffSetY);
-    }
-    
-    private void HandleAnimation()
-    {
         float targetMoveAnimSpeed = (_agent.enabled) ? _agent.velocity.normalized.magnitude : 0f;
         _currentMoveAnimSpeed = Mathf.Lerp(_currentMoveAnimSpeed,targetMoveAnimSpeed,moveSpeed * Time.deltaTime);
         animator.SetFloat(StringHolder.MoveInputAnimParam,_currentMoveAnimSpeed);
-
-        /*
-        if(!_isJumping && Physics.Raycast(transform.position,transform.forward,out RaycastHit hit,frontSideCheckDistance,offMeshLinkLayerMask.value))
-        {
-            _isJumping = true;
-            Debug.Log("Trigger Call on enemy jump up");
-            animator.SetTrigger(StringHolder.JumpTriggerAnimParameter);
-            //animator.ResetTrigger(StringHolder.JumpTriggerAnimParameter);
-        }
-        */
-        HandleFallingAnimation();
     }
 
     private void HandleShooting()
@@ -111,12 +96,6 @@ public class HumanEnemy : BaseEnemy
         }
     }
 
-    private void HandleFallingAnimation()
-    {
-        bool almostOnGround = Physics.CheckSphere(groundCheck.position,groundCheckDistance,groundLayerMask.value);
-        animator.SetBool(StringHolder.IsFallingAnimParameter,!almostOnGround);
-    }
-
     private void HandleFrontSide()
     {
         RaycastHit hit;
@@ -124,15 +103,5 @@ public class HumanEnemy : BaseEnemy
         int requiredMaskValue = ~(1 << gameObject.layer | collisionIgnoreLayerMask.value);
         _shouldStop = Physics.Raycast(transform.position,transform.forward,out hit,frontSideCheckDistance,
                       requiredMaskValue);
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        
-        int collidedLayerMask = 1 << other.gameObject.layer;
-        if(collidedLayerMask == offMeshLinkLayerMask.value)
-        {
-            animator.SetTrigger(StringHolder.JumpTriggerAnimParameter);
-        } 
     }
 }
--- Side Scrolling Shooting Game/Assets/Scripts/EnemyScripts/HumanEnemyScripts/HumanEnemyChaseState.cs
+++ Side Scrolling Shooting Game/Assets/Scripts/EnemyScripts/HumanEnemyScripts/HumanEnemyChaseState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HumanEnemyChaseState : IState
 {
     private HumanEnemy _humanEnemy;
+
+    private bool _jumpTrigger = false;
     public HumanEnemyChaseState(HumanEnemy humanEnemy)
     {
         _humanEnemy = humanEnemy;
@@ -22,6 +24,18 @@ public class HumanEnemyChaseState : IState
 
     public void OnUpdate()
     {
-        _humanEnemy.Agent.SetDestination(_humanEnemy.Target.position);   
+        _humanEnemy.Agent.SetDestination(_humanEnemy.Target.position);
+        if(_humanEnemy.Agent.isOnOffMeshLink && _jumpTrigger == false)
+        {
+            _jumpTrigger = true;
+            _humanEnemy.HumanAnimator.SetTrigger(StringHolder.JumpTriggerAnimParameter);
+            _humanEnemy.HumanAnimator.SetBool(StringHolder.IsFallingAnimParameter,true);
+        }
+
+        else if(_humanEnemy.Agent.isOnOffMeshLink == false && _jumpTrigger == true)
+        {
+            _jumpTrigger = false;
+            _humanEnemy.HumanAnimator.SetBool(StringHolder.IsFallingAnimParameter,false);
+        }
     }
 }
