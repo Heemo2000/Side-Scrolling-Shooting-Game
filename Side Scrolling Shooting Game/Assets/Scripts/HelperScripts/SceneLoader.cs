6b89b23604521223fa31011c85832af9e6555431
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-10-08 16:59:08 +0530

    Added loading logic.

---
+++ Side Scrolling Shooting Game/Assets/Scripts/HelperScripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        StartCoroutine(Load(name));
+    }
+
+    private IEnumerator Load(string name)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(name);
+        loadOperation.allowSceneActivation = false;
+
+        while(loadOperation.isDone == false)
+        {
+            Debug.Log("Load progress : " + loadOperation.progress);
+            if (loadOperation.progress >= 0.9f)
+            {
+                loadOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
