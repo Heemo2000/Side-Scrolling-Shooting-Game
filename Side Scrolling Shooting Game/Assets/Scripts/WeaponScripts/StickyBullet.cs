d1f9c1456fcb8d299a4ece29b67f65254799ab4b
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-10-26 17:36:28 +0530

    Fixed issues in powerups.

--- Side Scrolling Shooting Game/Assets/Scripts/WeaponScripts/StickyBullet.cs
+++ Side Scrolling Shooting Game/Assets/Scripts/WeaponScripts/StickyBullet.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class StickyBullet : NormalBullet
 {
@@ -10,13 +11,23 @@ public class StickyBullet : NormalBullet
     [SerializeField]private float stickDuration = 1.0f;
 
     [SerializeField]private ParticleSystem explosionEffect;
+    
     private Coroutine _stickAndExplodeCoroutine;
+    private bool _isHit;
+    private CinemachineImpulseSource _explosionImpulse;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _explosionImpulse = GetComponent<CinemachineImpulseSource>();
+    }
     private IEnumerator StickAndExplode(Collider other)
     {
         Stick(other);
         yield return new WaitForSeconds(stickDuration);
         DamageSurroundings();
         Destroy(gameObject);
+        yield return null;
     }
 
     private void Stick(Collider other)
@@ -29,26 +40,32 @@ public class StickyBullet : NormalBullet
 
     private void DamageSurroundings()
     {
+        //Debug.Log("Damaging surroundings");
         Collider[] detectedColliders = Physics.OverlapSphere(transform.position,damageRadius,~base.BulletData.ignoreMask.value);
         foreach(Collider collider in detectedColliders)
         {
-            Health health = collider.gameObject.GetComponent<Health>();
+            //Debug.Log("Detected " + collider.gameObject.name);
+            Health health = collider.GetComponent<Health>();
             health?.OnHealthDamaged?.Invoke(base.BulletData.damage);
         }
 
         Instantiate(explosionEffect,transform.position,Quaternion.identity);
+        _explosionImpulse.GenerateImpulse();
     }
     protected override void OnTriggerEnter(Collider other) 
     {
+        //Debug.Log("On Trigger Enter called");
         int colliderMaskValue = 1 << other.gameObject.layer;
         if((base.BulletData.ignoreMask.value & colliderMaskValue) != 0)
         {
+            //Debug.Log("Ignoring collision");
             Physics.IgnoreCollision(base.BulletCollider,other);
             return;
         }
-        if(_stickAndExplodeCoroutine == null)
+        if(_isHit == false)
         {
-          _stickAndExplodeCoroutine = StartCoroutine(StickAndExplode(other));  
+            _stickAndExplodeCoroutine = StartCoroutine(StickAndExplode(other));
+            _isHit = true;    
         }
 
     }
